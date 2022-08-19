cb933dfeb9c06229cbe473be793fa3c35d7a171a
josefran14 <fransaavedra21@gmail.com>
2022-08-19 15:50:31 -0400

    Ejercicios de la parte basica del test challenger

--- Js-challenger-basic/test-21/scripts.js
+++ Js-challenger-basic/test-21/scripts.js
@@ -1,18 +1,16 @@
-// const words = (a) => {
-//   return a
-//     .split("")
-//     .map((item, index) => String.fromCharCode( (a.charCodeAt(index) + 1))).join("")
-    
-// };
+const words = (a) => {
+  return a
+    .split("")
+    .map((item, index) => String.fromCharCode(a.charCodeAt(index) + 1))
+    .join("");
+};
 
-// console.log(words("bgddrd"));
+console.log(words("bgddrd"));
 
+// const words = (a) =>{
+//   let arrAbc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"]
+//   let arr2 = a.split("").map((item)=> arrAbc.indexOf(item))
+//    return arr2.map((item)=> item + 1).map((value) => arrAbc[value]).join("")
+// }
 
-
-const words = (a) =>{
-  let arrAbc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z"]
-  let arr2 = a.split("").map((item)=> arrAbc.indexOf(item))
-   return arr2.map((item)=> item + 1).map((value) => arrAbc[value]).join("")
-}
-
-console.log(words("bgddrd"))
+// console.log(words("bgddrd"))
